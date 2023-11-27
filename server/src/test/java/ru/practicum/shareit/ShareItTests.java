b2b426429550f41b52d46d2cc624712610463b6b
Straga07 <Straga07@yandex.ru>
2023-11-27 19:22:50 +0300

    update

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,7 +1,6 @@
 
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,12 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ShareItTests {
 
     @Test
-    public void testMainMethod() {
-        try {
-            ShareItServer.main(new String[]{});
-        } catch (Exception e) {
-            Assertions.fail("Exception thrown during execution: " + e.getMessage());
-        }
-
+    void contextLoads() {
     }
+
 }
